bf966b0f1abd42f2cdc11f686e2fb7ffcea07b78
amit.pa@cisinlabs.com <amit.pa@cisinlabs.com>
2022-05-24 17:06:56 +0530

    new component designed for view profile page

--- pages/student/view-profile/index.js
+++ pages/student/view-profile/index.js
@@ -10,14 +10,12 @@ import Typography from "@mui/material/Typography";
 const useStyles = makeStyles(() => ({
   form: {
     background: "#fff",
-    padding: "30px",
+    // padding: "30px",
+    marginTop: "30px",
     borderRadius: "10px",
   },
   userImg: {
-    width: "100px",
-    height: "100px",
-    borderRadius: "100%",
-    marginBottom: "5px",
+    borderRadius: "10px",
   },
   title: {
     fontSize: "0.990rem!important",
@@ -25,13 +23,50 @@ const useStyles = makeStyles(() => ({
     color: "#6792bf !important",
     lineHeight: "2.5rem !important",
     borderLeft: "3px solid #77aaec",
-    paddingLeft:" 10px",
+    paddingLeft: " 10px",
   },
   text: {
     fontSize: "0.990rem!important",
     color: "#7c7c7c !important",
     fontWeight: "500 !important",
     lineHeight: "2.5rem !important",
+  },
+  name_content: {
+    marginTop: "30px",
+  },
+  name: {
+    fontSize: "24px",
+    fontWeight: "500",
+    color: " #484444",
+    fontFamily: "sans-serif",
+    marginBottom: "0 !important",
+  },
+  email: {
+    fontSize: "14px",
+    color: " #696464",
+    fontFamily: "sans-serif",
+  },
+  content: {
+    marginTop: "20px !important",
+    padding: "0 30px !important",
+  },
+  content_second: {
+    marginTop: "5px !important",
+    padding: "0 30px !important",
+  },
+  sub_title: {
+    fontSize: "12px !important",
+    fontFamily: "sans-serif !important",
+    fontWeight: "500 !important",
+    color: "#a3a2a2 !important",
+    letterSpacing: "1px !important",
+    textAlign: "left",
+  },
+  sub_value: {
+    fontSize: "18px !important",
+    fontFamily: "sans-serif !important",
+    color: "#4a4646 !important",
+    textAlign: "left",
   },
 }));
 
@@ -52,14 +87,14 @@ const ViewProfile = () => {
             className={classes.form}
             component="form"
             sx={{
-              "& .MuiTextField-root": { m: 1, width: "25ch" },
+              "& .MuiTextField-root": { width: "25ch" },
               boxShadow: 4,
             }}
             noValidate
             autoComplete="off"
           >
-            <Grid container justifyContent={"center"} alignItems="center">
-              <Grid item>
+            <Grid container>
+              <Grid item lg={6} md={6}>
                 <div>
                   <img
                     className={classes.userImg}
@@ -68,131 +103,128 @@ const ViewProfile = () => {
                   />
                 </div>
               </Grid>
-            </Grid>
+              <Grid item lg={6} md={6}>
+                <div className={classes.name_content}>
+                  <Typography
+                    className={classes.name}
+                    variant="h5"
+                    gutterBottom
+                    component="div"
+                    textAlign={"center"}
+                  >
+                    Dima Blover
+                  </Typography>
+                  <Typography
+                    className={classes.email}
+                    variant="subtitle1"
+                    gutterBottom
+                    component="div"
+                    textAlign={"center"}
+                  >
+                    @dimablover
+                  </Typography>
+                </div>
+                <Grid container className={classes.content} spacing={4}>
+                  <Grid item lg={4} md={12} sm={12}>
+                    <div>
+                      <Typography
+                        className={classes.sub_title}
+                        variant="subtitle1"
+                        gutterBottom
+                        component="div"
+                      >
+                        Mobile
+                      </Typography>
+                      <Typography
+                        className={classes.sub_value}
+                        variant="subtitle1"
+                        gutterBottom
+                        component="div"
+                      >
+                        9127438447
+                      </Typography>
+                    </div>
+                  </Grid>
+                  <Grid item lg={4} md={12} sm={12}>
+                    <div>
+                      <Typography
+                        className={classes.sub_title}
+                        variant="subtitle1"
+                        gutterBottom
+                        component="div"
+                      >
+                        Parent's Name
+                      </Typography>
+                      <Typography
+                        className={classes.sub_value}
+                        variant="subtitle1"
+                        gutterBottom
+                        component="div"
+                      >
+                        Jack Blover
+                      </Typography>
+                    </div>
+                  </Grid>
+                  <Grid item lg={4} md={12} sm={12}>
+                    <div>
+                      <Typography
+                        className={classes.sub_title}
+                        variant="subtitle1"
+                        gutterBottom
+                        component="div"
+                      >
+                        Date Of Birth
+                      </Typography>
+                      <Typography
+                        className={classes.sub_value}
+                        variant="subtitle1"
+                        gutterBottom
+                        component="div"
+                      >
+                        23 April 2003
+                      </Typography>
+                    </div>
+                  </Grid>
+                </Grid>
 
-            <Grid container justifyContent={"center"} spacing={3}>
-              <Grid item>
-                <Typography
-                  color="primary"
-                  variant="subtitle1"
-                  gutterBottom
-                  component="div"
-                  className={classes.title}
-                >
-                  Email ID:
-                </Typography>
-                <Typography
-                  color="primary"
-                  variant="subtitle1"
-                  gutterBottom
-                  component="div"
-                  className={classes.title}
-                >
-                  Mobile Number:
-                </Typography>
-                <Typography
-                  color="primary"
-                  variant="subtitle1"
-                  gutterBottom
-                  component="div"
-                  className={classes.title}
-                >
-                  Parent Name:
-                </Typography>
-                <Typography
-                  color="primary"
-                  variant="subtitle1"
-                  gutterBottom
-                  component="div"
-                  className={classes.title}
-                >
-                  Parents Whatsapp Mobile Number:
-                </Typography>
-                <Typography
-                  color="primary"
-                  variant="subtitle1"
-                  gutterBottom
-                  component="div"
-                  className={classes.title}
-                >
-                  Students Name:
-                </Typography>
-                <Typography
-                  color="primary"
-                  variant="subtitle1"
-                  gutterBottom
-                  component="div"
-                  className={classes.title}
-                >
-                  Student Date Of Birth:
-                </Typography>
-                <Typography
-                  color="primary"
-                  variant="subtitle1"
-                  gutterBottom
-                  component="div"
-                  className={classes.title}
-                >
-                  Student's Grade in school:
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography
-                  className={classes.text}
-                  variant="subtitle1"
-                  gutterBottom
-                  component="div"
-                >
-                  rayn@mgail.com
-                </Typography>
-                <Typography
-                  className={classes.text}
-                  variant="subtitle1"
-                  gutterBottom
-                  component="div"
-                >
-                  32323242323
-                </Typography>
-                <Typography
-                  className={classes.text}
-                  variant="subtitle1"
-                  gutterBottom
-                  component="div"
-                >
-                  Rayn ronald
-                </Typography>
-                <Typography
-                  className={classes.text}
-                  variant="subtitle1"
-                  gutterBottom
-                  component="div"
-                >
-                  61728347384
-                </Typography>
-                <Typography
-                  className={classes.text}
-                  variant="subtitle1"
-                  gutterBottom
-                  component="div"
-                >
-                  Jack ronald
-                </Typography>
-                <Typography
-                  className={classes.text}
-                  variant="subtitle1"
-                  gutterBottom
-                  component="div"
-                >
-                  23 April 1999
-                </Typography>
-                <Typography
-                  className={classes.text}
-                  variant="subtitle1"
-                  gutterBottom
-                  component="div"
-                >
-                  A+
-                </Typography>
+                <Grid container className={classes.content_second} spacing={4}>
+                  <Grid item lg={4} md={12} sm={12}>
+                    <Typography
+                      className={classes.sub_title}
+                      variant="subtitle1"
+                      gutterBottom
+                      component="div"
+                    >
+                      Parent's Number
+                    </Typography>
+                    <Typography
+                      className={classes.sub_value}
+                      variant="subtitle1"
+                      gutterBottom
+                      component="div"
+                    >
+                      9127438447
+                    </Typography>
+                  </Grid>
+                  <Grid item lg={4} md={12} sm={12}>
+                    <Typography
+                      className={classes.sub_title}
+                      variant="subtitle1"
+                      gutterBottom
+                      component="div"
+                    >
+                      Grade
+                    </Typography>
+                    <Typography
+                      className={classes.sub_value}
+                      variant="subtitle1"
+                      gutterBottom
+                      component="div"
+                    >
+                      A, A++
+                    </Typography>
+                  </Grid>
+                </Grid>
               </Grid>
             </Grid>
           </Box>
